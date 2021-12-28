9f416d8fce32ef245ee8722c953c7daee8a33171
Vance McGrady <vancemcgrady@gmail.com>
2021-12-28 12:17:42 -0600

    operations functions complete.

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("testing");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operation, a, b) {
+  if (operation === "add") {
+    return add(a, b);
+  } else if (operation === "subtract") {
+    return subtract(a, b);
+  } else if (operation === "multiply") {
+    return multiply(a, b);
+  } else if (operation === "divide") {
+    return divide(a, b);
+  } else {
+    return "ERROR";
+  }
+}
+
+console.log(operate("subtract", 50, 5));
