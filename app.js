b26e24832c3c4f4a8e939149b9737896b2356a9f
Vance McGrady <vancemcgrady@gmail.com>
2021-12-29 11:28:29 -0600

    button's display numbers corretly.

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+const numBtns = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector("#AC");
+const currentNumberDisplay = document.querySelector(".current-number");
+const numberMemoryDisplay = document.querySelector(".number-memory");
+
+const calculator = {
+  displayValue: "",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+//add event listener to all number buttons
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", handleNumbers);
+});
+
+//add event listener to operator buttons
+operatorBtn.forEach((btn) => {
+  btn.addEventListener("click", handleOperators);
+});
+
+//add AC button event listener
+clearBtn.addEventListener("click", handleClear);
+
+function handleClear() {
+  currentNumberDisplay.innerText = "";
+  numberMemoryDisplay.innerText = "";
+}
+
+function handleNumbers(e) {
+  calculator.displayValue += e.target.innerText;
+  currentNumberDisplay.innerText += calculator.displayValue;
+  console.log(calculator.displayValue);
+}
+
+function handleOperators(e) {
+  currentNumberDisplay.innerText += e.target.innerText;
+}
+
 function add(a, b) {
   return a + b;
 }
