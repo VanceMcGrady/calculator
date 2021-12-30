8b8e6a62e30290c4624c9661a91cfc85a51e1ed2
Vance McGrady <vancemcgrady@gmail.com>
2021-12-30 17:25:23 -0600

    number buttons working and updating display

--- app.js
+++ app.js
@@ -1,104 +1,90 @@
-const numBtns = document.querySelectorAll(".number");
-const operatorBtn = document.querySelectorAll(".operator");
-const clearBtn = document.querySelector("#AC");
-const currentNumberDisplay = document.querySelector(".current-number");
-const numberMemoryDisplay = document.querySelector(".number-memory");
+//DOM variable declaration
+const numberBtns = document.querySelectorAll(".number");
+const operatorBtns = document.querySelectorAll(".operator");
+const allClearBtn = document.querySelector("#AC");
+const deleteBtn = document.querySelector("#DEL");
 const equalsBtn = document.querySelector(".equals");
-const decimalBtn = document.querySelector(".decimal");
 
+const numberMemoryDisplay = document.querySelector(".number-memory");
+const currentNumberDisplay = document.querySelector(".current-number");
+
+// calc object
 const calculator = {
-  displayValue: "",
   firstOperand: "",
-  waitingForSecondOperand: false,
-  operator: null,
+  currentOperand: "",
+  operation: "",
 };
 
-//add event listener to all number buttons
-numBtns.forEach((btn) => {
+//functions
+function updateDisplay() {
+  numberMemoryDisplay.innerText = calculator.firstOperand;
+  currentNumberDisplay.innerText = calculator.currentOperand;
+}
+function handleNumbers(e) {
+  calculator.currentOperand += e.target.innerText;
+  updateDisplay();
+}
+
+function allClear() {
+  calculator.firstOperand = "";
+  calculator.currentOperand = "";
+  calculator.operation = "";
+}
+
+function operate(operator, a, b) {
+  let numOne = parseFloat(a);
+  let numTwo = parseFloat(b);
+
+  function add(...params) {
+    let sum = 0;
+    params.forEach((item) => {
+      sum = sum + item;
+    });
+    return sum;
+  }
+
+  function subtract(...params) {
+    let difference = 0;
+    params.forEach((item) => {
+      difference = params[0] - params[1];
+    });
+    return difference;
+  }
+
+  function multiply(...params) {
+    let product = 1;
+    params.forEach((item) => {
+      product = product * item;
+    });
+    return product;
+  }
+
+  function divide(...params) {
+    if (params.includes(0)) {
+      return "ERR";
+    } else {
+      let quotient = 0;
+      quotient = params[0] / params[1];
+      return quotient;
+    }
+  }
+
+  switch (operator) {
+    case "+":
+      return add(numOne, numTwo);
+      break;
+    case "-":
+      return subtract(numOne, numTwo);
+      break;
+    case "x":
+      return multiply(numOne, numTwo);
+      break;
+    case "/":
+      return divide(numOne, numTwo);
+  }
+}
+
+//adding event listeners
+numberBtns.forEach((btn) => {
   btn.addEventListener("click", handleNumbers);
 });
-
-//add event listener to operator buttons
-operatorBtn.forEach((btn) => {
-  btn.addEventListener("click", handleOperators);
-});
-
-//add AC button event listener
-clearBtn.addEventListener("click", handleClear);
-
-//add equals button event listener
-equalsBtn.addEventListener("click", equals);
-
-//add decimal button event listener
-decimalBtn.addEventListener("click", handleDecimal);
-
-//decimal
-function handleDecimal(e) {
-  if (calculator.displayValue.includes(".")) {
-    null;
-  } else {
-    calculator.displayValue += e.target.innerText;
-    currentNumberDisplay.innerText = calculator.displayValue;
-  }
-}
-
-//numbers
-function handleNumbers(e) {
-  calculator.displayValue += e.target.innerText;
-  currentNumberDisplay.innerText = calculator.displayValue;
-  console.log(calculator.displayValue);
-}
-
-//operators
-function handleOperators(e) {
-  calculator.firstOperand += `${calculator.displayValue} ${e.target.innerText} `;
-  numberMemoryDisplay.innerText = calculator.firstOperand;
-  calculator.displayValue = "";
-  currentNumberDisplay.innerText = calculator.displayValue;
-}
-
-// equals
-function equals(e) {
-  calculator.firstOperand += `${calculator.displayValue}`;
-  solution = calculator.firstOperand.split(" ");
-
-  console.log(solution);
-}
-
-//clear
-function handleClear() {
-  calculator.displayValue = "";
-  calculator.firstOperand = "";
-  currentNumberDisplay.innerText = "";
-  numberMemoryDisplay.innerText = "";
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(operation, a, b) {
-  if (operation === "add") {
-    return add(a, b);
-  } else if (operation === "subtract") {
-    return subtract(a, b);
-  } else if (operation === "multiply") {
-    return multiply(a, b);
-  } else if (operation === "divide") {
-    return divide(a, b);
-  } else {
-    return "ERROR";
-  }
-}
